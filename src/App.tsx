54d34a5e82fc4c8984f3c45794f16b607cc3bc20
Amal C P <acp070146@gmail.com>
2023-12-12 09:53:20 +0530

    basic pages defined withroying

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from './Pages/Home/Home';
+import { AboutUs } from './Pages/AboutUs/AboutUs';
+import { Volunteer } from './Pages/Volunteer/Volunteer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/aboutus",
+      element: <AboutUs />,
+    },
+    {
+      path: "/volunteer",
+      element: <Volunteer />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+
+type Props = {}
+
+export const Navbar = (_props: Props) => {
+  return (
+    <div>Navbar</div>
+  )
+}
