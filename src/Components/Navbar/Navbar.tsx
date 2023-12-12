403dd60f2863887223cee141fee304a3fe5272cc
Amal C P <acp070146@gmail.com>
2023-12-12 19:48:35 +0530

    navbar initalised

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,8 +1,95 @@
-
-type Props = {}
+import styles from "./Navbar.module.css";
+import logo from "../../../loader.png";
+import { useState, useEffect } from "react";
+import { useReactPath } from "./path.hook";
+import { MdMenu } from "react-icons/md";
+type Props = {};
 
 export const Navbar = (_props: Props) => {
+  const [openmenu, setopenmenu] = useState(false);
+  const [navbg, setNavBg] = useState(false);
+  function openMenu() {
+    setopenmenu(!openmenu);
+  }
+  const path = useReactPath();
+  const navContent = ["Home", "about Us", "Our Work", "Join Us", "Our Blog"];
+  useEffect(() => {}, [path]);
+  const changeNavBg = () => {
+    window.scrollY >= 150 ? setNavBg(true) : setNavBg(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavBg);
+    return () => {
+      window.removeEventListener("scroll", changeNavBg);
+    };
+  }, []);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      className={styles.navbarWrapper}
+      style={{ background: navbg ? "rgba(255,255,255,0.4)" : "transparent" }}
+    >
+      <div className={styles.navbarLeft}>
+        <a href="#home">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <div className={styles.navbarRight}>
+        <div>
+          {navContent.map((content, i) => (
+            <a
+              href={`#${content.toLowerCase().replace(/\s+/g, "")}`}
+              key={i.toString() + content}
+            >
+              <p
+                style={{
+                  height: "12px",
+                  fontSize: "18px",
+                  fontWeight: 600,
+                  color: window.location.href.includes(
+                    `#${content.toLowerCase().replace(/\s+/g, "")}`
+                  )
+                    ? "#05B570"
+                    : "#696969",
+                }}
+              >
+                {content}
+              </p>
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={styles.navbarMobile}>
+        <button onClick={openMenu}>
+          <MdMenu />
+        </button>
+        {openmenu && (
+          <div>
+            {navContent.map((content, i) => (
+              <a
+                href={`#${content.toLowerCase().replace(/\s+/g, "")}`}
+                key={i.toString() + content}
+              >
+                <p
+                  style={{
+                    color: window.location.href.includes(
+                      `#${content.toLowerCase().replace(/\s+/g, "")}`
+                    )
+                      ? "#05B570"
+                      : "#696969",
+                    height: "12px",
+                  }}
+                >
+                  {content}
+                </p>
+              </a>
+            ))}
+          </div>
+        )}
+      </div>{" "}
+      <button>
+        <a href="http://app.mulearn.org">Study Now</a>
+      </button>
+    </div>
+  );
+};
