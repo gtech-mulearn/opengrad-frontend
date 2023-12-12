9fb9f220c8776bfe167651eb16f81fdbeeee2939
Amal C P <acp070146@gmail.com>
2023-12-12 10:21:55 +0530

    basic structure done

--- src/Pages/AboutUs/Components/AdvisoryPanel/AdvisoryPanel.tsx
+++ src/Pages/AboutUs/Components/AdvisoryPanel/AdvisoryPanel.tsx
@@ -1,9 +1,17 @@
+import { ImageHoverBox } from "../../../../Components/ImageHoverBox/ImageHoverBox";
 
 
 type Props = {}
 
 export const AdvisoryPanel = (_props: Props) => {
   return (
-    <div>AdvisoryPanel</div>
-  )
+    <div>
+      <h1>Advisory Panel</h1>
+      <div>
+        <ImageHoverBox />
+        <ImageHoverBox />
+        <ImageHoverBox />
+      </div>
+    </div>
+  );
 }
