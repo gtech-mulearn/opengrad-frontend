45b5a4528e80e5dad295487afd06fc55c684552f
Amal C P <acp070146@gmail.com>
2023-12-16 21:03:23 +0530

    basic section of problem Scenario completed

--- src/Pages/Home/Components/ProblemScenario/ProblemScenario.tsx
+++ src/Pages/Home/Components/ProblemScenario/ProblemScenario.tsx
@@ -1,39 +1,46 @@
 import { SectionHeading } from "../../../../Components/SectionHeading/SectionHeading";
-import styles from './ProblemScenario.module.css'
-type Props = {};
+import styles from "./ProblemScenario.module.css";
+import LOA from "./assets/LOA.png";
+import LOM from "./assets/LOM.png";
+import accessability from "./assets/accessability.png";
+import finance from "./assets/finance.png";
+import graph from "./assets/graphmode.png";
+type Props = {
+  image: string;
+  pra: string;
+};
 
 export const ProblemScenario = (_props: Props) => {
   return (
     <div className={styles.ProblemScenarioWrapper}>
-      <div>
+      <div className={styles.heading}>
         {" "}
         <SectionHeading title="The Problem Scenario" />
         <p>
-          Increased commercialization of entrance education, has led to
+          Increased commercialization of entrance education, has<br></br> led to
           disproportionate participation.
         </p>
       </div>
-      <div>
-        <div>
-          <div><GridSet /></div>
-          <div>svg,txt</div> <div>svg,txt</div> <div>svg,txt</div>
+      <div className={styles.DetailSection}>
+        <div className={styles.GridSetWrapper}>
+          <GridSet image={LOA} pra={"Lack of awareness"} />
+          <GridSet image={LOM} pra={"Lack of Mentors"} />
+          <GridSet image={accessability} pra={"Accessibility"} />
+          <GridSet image={finance} pra={"Finance"} />
         </div>
-        <div>
-          <div>outer circle</div>
-          <div>inner circle</div>
-          <div>line 1</div>
-          <div>line 2</div>
+        <div className={styles.CircularAnimationWrapper}>
+          <img src={graph} alt="" />
         </div>
       </div>
     </div>
   );
 };
 
-
-const GridSet =()=>{
-  return(
-    <div>
-
+const GridSet = ({ image, pra }: Props) => {
+  return (
+    <div className={styles.GrifSet}>
+      <img src={image} alt="" />
+      <p>{pra}</p>
     </div>
-  )
-}
+  );
+};
