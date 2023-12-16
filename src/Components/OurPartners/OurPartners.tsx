eb8728f58794778cd2bf9855302689521ff628b9
Amal C P <acp070146@gmail.com>
2023-12-17 01:23:41 +0530

    partners section update

--- src/Components/OurPartners/OurPartners.tsx
+++ src/Components/OurPartners/OurPartners.tsx
@@ -1,12 +1,27 @@
 import { SectionHeading } from "../SectionHeading/SectionHeading";
+import styles from './OurPartners.module.css'
 
 
-type Props = {}
+type Partner = {
+  image: string;
+  name: string;
+};
 
-export const OurPartners = (_props: Props) => {
+type OurPartnersProps = {
+  partners: Partner[];
+};
+
+export const OurPartners = ({ partners }: OurPartnersProps) => {
   return (
-    <div>
+    <div className={styles.Partners}>
       <SectionHeading title="Our Partners" />
+      <div className={styles.PartnersImageWrapper}>
+        {partners.map((partner, index) => (
+          <div key={index}>
+            <img src={partner.image} alt={partner.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
