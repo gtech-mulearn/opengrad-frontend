05a8b1db65203bfb20802c91f8be5916414a282e
Amal C P <acp070146@gmail.com>
2023-12-28 20:45:49 +0530

    comming soon page added

---
+++ src/Pages/OurWork/OurWork.tsx
@@ -0,0 +1,9 @@
+import styles from './OurWork.module.css'
+
+type Props = {}
+
+export const OurWork = (_props: Props) => {
+  return (
+    <div className={styles.ourworkwrapper}>OurWork</div>
+  )
+}
