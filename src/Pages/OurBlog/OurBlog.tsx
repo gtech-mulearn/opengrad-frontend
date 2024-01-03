7fdc5a56a4cac7dcb8cba022aa93606babf82eb0
Amal C P <acp070146@gmail.com>
2024-01-04 00:49:09 +0530

    our blog update

--- src/Pages/OurBlog/OurBlog.tsx
+++ src/Pages/OurBlog/OurBlog.tsx
@@ -1,51 +1,85 @@
+import { Footer } from "../../Components/Footer/Footer";
+import { Navbar } from "../../Components/Navbar/Navbar";
 import { getBlogs } from "./Api";
 import styles from "./OurBlog.module.css";
 import { useState, useEffect } from "react";
 
-
 type Props = {};
 
 export const OurBlog = (_props: Props) => {
-    const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<any[]>([]);
 
-    const handleFetchDetails = async () => {
-      try {
-        const response = await getBlogs();
-        if (response) {
-          setData(response);
-          console.log(data);
-        }
-      } catch (error) {}
-    };
+  const handleFetchDetails = async () => {
+    try {
+      const response = await getBlogs();
+      if (response) {
+        setData(response);
+        console.log(data);
+      }
+    } catch (error) {}
+  };
 
-    useEffect(() => {
-      handleFetchDetails();
-    }, []);
+  useEffect(() => {
+    handleFetchDetails();
+  }, []);
   return (
-    <div>
-      <div>
-        <h1>Our Blogs</h1>
-      </div>
-      <div>
-        <div className={styles.TopInner}>
-          <p>Our Blogs</p>
+    <div className={styles.Wrapper}>
+      <Navbar />
+      <div className={styles.HeaderWrapper}>
+        <div className={styles.Header}>
+          <div className={styles.BackgroundText}>
+            <h1>Our Blogs</h1>
+          </div>
+          <div className={styles.DetailWrapper}>
+            <div className={styles.TopInner}>
+              <p>Our Blogs</p>
+            </div>
+            <div className={styles.category}>
+              <button>News and Updates</button>
+              <button>Events</button>
+              <button>Campus Diaries</button>
+              <button>Insights</button>
+            </div>
+          </div>
         </div>
         <div>
-          <button>News and Updates</button>
-          <button>Events</button>
-          <button>Campus Diaries</button>
-          <button>Insights</button>
+          {data.slice(0, 1).map(({ image, title, description, category }) => {
+            return (
+              <div>
+                <img src={image} alt="" />
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <div>
+                  <p>{category}</p>
+                </div>
+              </div>
+            );
+          })}
+          <div>
+            {data.slice(1, 3).map(({ image, title, description, category }) => {
+              return (
+                <div>
+                  <img src={image} alt="" />
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <div>
+                    <p>{category}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div>
         <h1>All Blogs</h1>
         <div>
-          {data.map(({ image, title, para, category }) => {
+          {data.map(({ image, title, description, category }) => {
             return (
               <div>
                 <img src={image} alt="" />
                 <h3>{title}</h3>
-                <p>{para}</p>
+                <p>{description}</p>
                 <div>
                   <p>{category}</p>
                 </div>
@@ -54,6 +88,7 @@ export const OurBlog = (_props: Props) => {
           })}
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
