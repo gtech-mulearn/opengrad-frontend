37982d8cbdea4138ba542f337285c3e112dc7ccb
Amal C P <acp070146@gmail.com>
2024-01-04 02:02:39 +0530

    top and mid section ui completed in our blog

--- src/Pages/OurBlog/OurBlog.tsx
+++ src/Pages/OurBlog/OurBlog.tsx
@@ -1,5 +1,6 @@
 import { Footer } from "../../Components/Footer/Footer";
 import { Navbar } from "../../Components/Navbar/Navbar";
+import { ArrowRight } from "../../assets/svg";
 import { getBlogs } from "./Api";
 import styles from "./OurBlog.module.css";
 import { useState, useEffect } from "react";
@@ -88,19 +89,45 @@ export const OurBlog = (_props: Props) => {
       <div className={styles.allBlogs}>
         <h1>All Blogs</h1>
         <div className={styles.blogWrapper}>
-          {[...data].reverse().map(({ image, title, description, category }) => {
-            return (
-              <div className={styles.individualDiv}>
-                <img src={image} alt="" />
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <div>
-                  <p>{category}</p>
+          {[...data]
+            .reverse()
+            .map(({ image, title, description, category }) => {
+              return (
+                <div className={styles.individualDiv}>
+                  <img src={image} alt="" />
+                  <h3>{title}</h3>
+                  <p>{description}</p>
+                  <div className={styles.categoryDiv}>
+                    <p
+                      className={
+                        styles.newsAndUpdates + " " + styles.contentCategory
+                      }
+                    >
+                      {category}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
+        <button>View More</button>
+      </div>
+      <div>
+        <div>
+          <h1>
+            <p>Join us in</p>&nbsp;<span>rebuilding the future</span>&nbsp;
+            <p>of Indian Education</p>
+          </h1>
+          <p>
+            Help us build resources, mentor aspirants and support our activities
+            and events.
+          </p>
+          <button>
+            <p>Join Us</p>
+            <ArrowRight color="rgba(3, 72, 82, 1)" />
+          </button>
+        </div>
+        <img src="" alt="" />
       </div>
       <Footer />
     </div>
