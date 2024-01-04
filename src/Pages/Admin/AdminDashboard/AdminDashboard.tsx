b99d986f448e4f17ab163fd4e8d132db0d3c6b04
Amal C P <acp070146@gmail.com>
2024-01-04 08:16:48 +0530

    admin blog page inital section completed

--- src/Pages/Admin/AdminDashboard/AdminDashboard.tsx
+++ src/Pages/Admin/AdminDashboard/AdminDashboard.tsx
@@ -1,17 +1,51 @@
+import { useState } from "react";
 import { Blogs } from "./Blogs";
+import styles from "./AdminDashboard.module.css";
+import { HomeTestimonial } from "./HomeTestimonial";
+import { JoinUsTestimonial } from "./JoinUsTestimonial";
 
 type Props = {};
 
 export const AdminDashboard = (_props: Props) => {
+  const [activeComponent, setActiveComponent] = useState<string>("Blogs");
+  const data = [
+    {
+      name: "Blogs",
+      value: "Blogs",
+    },
+    {
+      name: " Home - Testimonials",
+      value: "HomeTestimonial",
+    },
+    {
+      name: "Join Us - Testimonial",
+      value: "JoinUsTestimonial",
+    },
+  ];
   return (
-    <div>
-      <h2>AdminDashboard</h2>
-      <div>
-        <button>Blogs</button>
-        <button>Home - Testimonials</button>
-        <button>Join Us - Testimonial</button>
+    <div className={styles.AdminDashboard}>
+      <div className={styles.head}>
+        <h1>AdminDashboard</h1>
+        <a href="/home">Back To Home</a>
       </div>
-      <Blogs />
+      <div className={styles.buttonWrapper}>
+        {data.map(({ name, value }) => {
+          return (
+            <button
+              style={{
+                backgroundColor:
+                  activeComponent == value ? "#034852" : "#667085",
+              }}
+              onClick={() => setActiveComponent(value)}
+            >
+              {name}
+            </button>
+          );
+        })}
+      </div>
+      {activeComponent === "Blogs" && <Blogs />}
+      {activeComponent === "HomeTestimonial" && <HomeTestimonial />}
+      {activeComponent === "JoinUsTestimonial" && <JoinUsTestimonial />}
     </div>
   );
 };
