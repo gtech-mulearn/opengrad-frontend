64036c015b97650fd3759a66260bee4f96eeb0dc
Amal C P <acp070146@gmail.com>
2024-02-15 18:03:07 +0530

    image and content update in header

--- src/Pages/Home/Components/CounterContainer/CounterContainer.tsx
+++ src/Pages/Home/Components/CounterContainer/CounterContainer.tsx
@@ -13,7 +13,7 @@ export const CounterContainer = (_props: Props) => {
         text1="Students"
         text2="Reached"
         speed={0}
-        start={1500}
+        start={2000}
       />{" "}
       <CounterComponent
         Svg={Personsvg}
@@ -21,14 +21,14 @@ export const CounterContainer = (_props: Props) => {
         text1="Students"
         text2="Enrolled"
         speed={10}
-        start={50}
+        start={100}
       />{" "}
       <CounterComponent
         Svg={Timersvg}
         limit={200}
         text1="Hours of"
         text2="content"
-        speed={40}
+        speed={30}
         start={0}
       />
     </div>
